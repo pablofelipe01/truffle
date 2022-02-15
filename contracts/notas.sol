d4077014769de877b419dfa6f8f34469445acf77
Pablo Acebedo <pablofelipe@me.com>
2022-02-15 14:55:29 -0500

    test complete

--- contracts/notas.sol
+++ contracts/notas.sol
@@ -13,53 +13,84 @@ pragma experimental ABIEncoderV2;
 //  Alba   |    98765Z    |      5
 
 contract notas {
+    // Direccion del profesor
     address public profesor;
 
+    // Constructor
     constructor() public {
         profesor = msg.sender;
     }
 
+    // Mapping para relacionar el hash de la identidad del alumno con su nota del examen
     mapping(bytes32 => uint256) Notas;
 
-    string[] revisiones;
+    // Mapping de los alumnos de pidan revisiones de examen para una asignatura
+    mapping(string => string[]) revisiones;
 
+    // Eventos
     event alumno_evaluado(bytes32);
     event evento_revision(string);
 
-    function Evaluar(string memory _idAlumno, uint256 _nota)
-        public
-        UnicamenteProfesor(msg.sender)
-    {
-        bytes32 hash_idAlumno = keccak256(abi.encode(_idAlumno));
+    // Funcion para evaluar a alumnos
+    function Evaluar(
+        string memory _asignatura,
+        string memory _idAlumno,
+        uint256 _nota
+    ) public UnicamenteProfesor(msg.sender) {
+        // Hash de la identificacion del alumno
+        bytes32 hash_idAlumno = keccak256(
+            abi.encodePacked(_asignatura, _idAlumno)
+        );
+        // Relacion entre el hash de la identificacion del alumno y su nota
         Notas[hash_idAlumno] = _nota;
+        // Emision del evento
         emit alumno_evaluado(hash_idAlumno);
     }
 
+    // Control de las funciones ejecutables por el profesor
     modifier UnicamenteProfesor(address _direccion) {
+        // Requiere que la direccion introducido por parametro sea igual al owner del contrato
         require(
             _direccion == profesor,
-            "No tiene permiso para ejecutar esta funcion"
+            "No tienes permisos para ejecutar esta funcion."
         );
         _;
     }
 
-    function VerNotas(string memory _idAlumno) public view returns (uint256) {
-        bytes32 hash_idAlumno = keccak256(abi.encode(_idAlumno));
+    // Funcion para ver las notas de un alumno
+    function VerNotas(string memory _asignatura, string memory _idAlumno)
+        public
+        view
+        returns (uint256)
+    {
+        // Hash de la identificacion del alumno
+        bytes32 hash_idAlumno = keccak256(
+            abi.encodePacked(_asignatura, _idAlumno)
+        );
+        // Nota asociada al hash del alumno
         uint256 nota_alumno = Notas[hash_idAlumno];
+        // Visualizar la nota
         return nota_alumno;
     }
 
-    function Revision(string memory _idAlumno) public {
-        revisiones.push(_idAlumno);
+    // Funcion para pedir una revision del examen
+    function Revision(string memory _asignatura, string memory _idAlumno)
+        public
+    {
+        // Almacenamiento de la identidad del alumno en un array
+        revisiones[_asignatura].push(_idAlumno);
+        // Emision del evento
         emit evento_revision(_idAlumno);
     }
 
-    function VerRevisiones()
+    // Funcion para ver los alumnos que han solicitado revision de examen
+    function VerRevisiones(string memory _asignatura)
         public
         view
         UnicamenteProfesor(msg.sender)
         returns (string[] memory)
     {
-        return revisiones;
+        // Devolver las identidades de los alumnos
+        return revisiones[_asignatura];
     }
 }
